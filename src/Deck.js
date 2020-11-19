492cc276dcfc9a56bc213c812ae05f10437bed7d
unknown <jreidmke@gmail.com>
2020-11-19 11:12:27 -0600

    init Deck.js

---
+++ src/Deck.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState, useRef } from "react";
+import Card from "./Card";
+import axios from "axios";
+
+const BASEURL = `https://deckofcardsapi.com/api/deck/`;
+
+const Deck = () => {
+    
+}
