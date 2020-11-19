543621fe93acf92f22533f6eadb463eccdcde72a
unknown <jreidmke@gmail.com>
2020-11-19 11:12:18 -0600

    completed card

--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,11 @@
 import React, { useState, useEffect } from 'react';
+
+//ok. a card has a suit and a value. we'll make our cards an li that displays both
+
+const Card = ({suit, value}) => {
+    return(
+        <li>This card is the {value} of {suit}.</li>
+    )
+}
+
+export default Card;
