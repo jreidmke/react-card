ef3777d17b677ed628d4da7d09b644b55a8c28c4
unknown <jreidmke@gmail.com>
2020-11-21 11:10:21 -0600

    completed timer portion

--- src/Deck.js
+++ src/Deck.js
@@ -19,7 +19,7 @@ const Deck = () => {
         getDeck() //call function
     }, [setDeck])//stipulate getData should only be called when setDeck is changed. remember, set deck is called upon initial load. (All states are).
 
-//ok, so not too crazy. we literally just need to wrap this thing up in a timer with some conditionals to check wether or not auto is on.
+//ok, so not too crazy. we literally just need to wrap this thing up in a timer with some conditionals to check wether or not auto is on. this requires wrapping the statement in a useEffect as otherwise it would rerender the timer every time a new card is called.
 
     async function getCard() {
         try {
@@ -28,7 +28,6 @@ const Deck = () => {
                 throw new Error("all out!");
             }
             const card = draw.data.cards[0];
-            console.log(card);
 
             setDrawn(d => {
                 return [
@@ -48,13 +47,54 @@ const Deck = () => {
         await getCard();
     }
 
+    useEffect(() => {
+        async function getCard() {
+            try{
+                let drawRes = await axios.get(`${BASEURL}/${deck.deck_id}/draw`);
+                if(drawRes.data.remaining === 0) {
+                    setAuto(false);
+                    throw new Error("all out");
+                }
+
+                const card = drawRes.data.cards[0];
+                setDrawn(d => [
+                    ...d,
+                    {
+                        id: card.code,
+                        suit: card.suit,
+                        value: card.value
+                    }
+                ]);
+            } catch(error) {
+                alert(error);
+            }
+        }
+
+        if(auto && !timerRef.current) {
+            timerRef.current = setInterval(async () => {
+                await getCard();
+            }, 1000);
+        }
+
+        return () => {
+            clearInterval(timerRef.current);
+            timerRef.current = null;
+        }
+
+    }, [auto, setAuto, deck]);
+
+    const toggleAuto = () => {
+        setAuto(auto => !auto);
+    };
+
+
     const cards = drawn.map(c => ( //cards is a visual represenation of the drawn cards. Use Card components to create an array and store em in the variable.
         <Card key={c.id} suit={c.suit} value={c.value} />
     ));
 
     return(
         <ul>
-            <button onClick={drawCard}>Draw a card</button>
+            <button onClick={toggleAuto}>Draw a card</button>
             {cards}
         </ul>
     )
